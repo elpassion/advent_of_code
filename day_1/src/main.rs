8251157156fe9702097a3beaa7ce24ef4233bf60
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2017-12-07 14:58:05 +0100

    Make it work

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -2,7 +2,6 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
-    // ...snip...
     let filename = "input.txt";
     println!("In file {}", filename);
 
@@ -16,24 +15,25 @@ fn main() {
     println!("With text:\n{}", result);
 }
 
-pub fn adder(input: &str) -> u32 {
+pub fn adder(input: String) -> u32 {
     let mut sum = 0;
+
     let numbers: Vec<u32> = input.chars()
-        .map(|c| c.to_digit(10).unwrap())
+        .filter_map(|c| c.to_digit(10))
         .collect();
 
     for n in 0..numbers.len() {
         let mut next_index = n + 1;
-        
+
         if n == numbers.len() - 1 {
             next_index = 0;
         }
-        
+
         if numbers[n] == numbers[next_index] {
             sum += numbers[n];
         }
     }
-    
+
     return sum;
 }
 
@@ -43,26 +43,36 @@ mod tests {
 
     #[test]
     fn test_one_one() {
-        assert_eq!(1, adder("1"));
+        assert_eq!(1, adder(String::from("1")));
     }
 
     #[test]
     fn test_1122() {
-      assert_eq!(3, adder("1122"));
+      assert_eq!(3, adder(String::from("1122")));
     }
-    
+
     #[test]
     fn test_1111() {
-      assert_eq!(4, adder("1111"));
+      assert_eq!(4, adder(String::from("1111")));
     }
-    
+
     #[test]
     fn test_1234() {
-      assert_eq!(0, adder("1234"));
+      assert_eq!(0, adder(String::from("1234")));
     }
-    
+
     #[test]
     fn test_91212129() {
-      assert_eq!(9, adder("91212129"));
+      assert_eq!(9, adder(String::from("91212129")));
+    }
+
+    #[test]
+    fn test_1() {
+        assert_eq!(3, adder(String::from("111")));
+    }
+
+    #[test]
+    fn test_2() {
+        assert_eq!(4, adder(String::from("22")));
     }
 }
